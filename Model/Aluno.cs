0545fd45f40cb5f47e1e7cc68765864bff6b5048
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-08-29 22:04:19 -0300

    Aula 1 POO Classes e Objetos

---
+++ Model/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1.Model
+{
+    class Aluno
+    {
+        //Caracteristicas de uma classe (atributos)
+        public string nome;
+        public int idade;
+        
+        public void imprimiNome()
+        {
+            Console.WriteLine(nome + " Idade " + idade);
+        }
+        //Operações ou funcionalidades (Métodos)
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Aula_1.Model; //caminho da pasta model(Insira os pacotes da pasta Model, ou seja, da classe criada)
+
+namespace Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//instanciar um objeto:criando o objeto | Passa a existir fisicamente
+            //Classe(Aluno) nomeObjeto(alu) = new construtor | new (operador de instanciação)
+            Aluno alu = new Aluno();
+            alu.nome = "Gaspar";
+            alu.idade = 2;
+
+            Aluno alu2 = new Aluno();
+            alu2.nome = "Uriel";
+            alu2.idade = 6;
+
+            Aluno alu3 = new Aluno();
+            alu3.nome = "Sarah";
+            alu3.idade = 3;
+            alu.imprimiNome();
+            alu2.imprimiNome();
+            alu3.imprimiNome();
+        }
+    }
+}
